7f7d52bd81898da35808da57d29024b88bcc4806
RibbonEyes1 <Joceline.marinhernandez25@ycdsbk1.ca>
2024-03-05 10:59:39 -0500

    Wip (NOt done at all, needs more time)

--- quiz.py
+++ quiz.py
@@ -1,6 +1,16 @@
 """
 File: quiz.py
-Author: Name
-Date: YYYY-MM-DD
-Description: A brief explanation of what this program does.
+Author: Joceline Marin_Hernandez
+Date: 2024-March-5
+Description: a fun little quiz?
 """
+#a fun multiple choice quiz
+
+#asks a question about a book, and needs user input
+print("-----------------------------------------------------)")
+print("1. Who wrote the book, 'Crime and Punishment?' \
+      Fyodor Dostoevsky \
+      Herman Hesse \
+      Lucy Maude Montgomery\
+      Herman Melville" )
+
